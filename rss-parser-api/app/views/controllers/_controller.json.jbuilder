aeae4d2a5827a9f024c4fc8d4129d38dd86fc58b
Lascau Sebastian <lascauionutsebastian98@gmail.com>
2023-04-27 20:16:03 +0300

    generated controller + added gems

--- rss-parser-api/app/views/controllers/_controller.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! controller, :id, :News, :created_at, :updated_at
-json.url controller_url(controller, format: :json)
--- rss-parser-api/db/schema.rb
+++ rss-parser-api/db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_165337) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_170536) do
   create_table "controllers", force: :cascade do |t|
     t.string "News"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "news", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
